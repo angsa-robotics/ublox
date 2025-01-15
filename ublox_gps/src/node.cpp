625ff3b3da41fa2a1ec5b6dbc2babe2b62965538
Angsa Deployment Team <team@angsa-robotics.com>
2025-01-15 13:18:17 +0100

    Revert "finished configuring parameter configuration for cno threshold and min elevation"
    This reverts commit 0c4c5430c9787f0aff7471578583b15182d1ca21.

--- ublox_gps/src/node.cpp
+++ ublox_gps/src/node.cpp
@@ -474,10 +474,6 @@ void UbloxNode::getRosParams() {
 
   this->declare_parameter("diagnostic_period", kDiagnosticPeriod);
 
-  // Angsa fork parameters
-  this->declare_parameter("min_elevation", 0);
-  this->declare_parameter("cno_threshold", 20);
-
   // Create publishers based on parameters
   if (getRosBoolean(this, "publish.nav.status")) {
     nav_status_pub_ = this->create_publisher<ublox_msgs::msg::NavSTATUS>("navstatus", 1);
@@ -801,16 +797,6 @@ bool UbloxNode::configureUblox() {
       }
       if (getRosBoolean(this, "dat.set") && !gps_->configure(cfg_dat_)) {
         throw std::runtime_error("Failed to set user-defined datum.");
-      }
-      // Set Angsa fork parameters
-      int8_t min_elevation = this->get_parameter("min_elevation").get_value<int8_t>();
-      uint8_t cno_threshold = this->get_parameter("cno_threshold").get_value<uint8_t>();
-
-      if (!gps_->setMinElev(min_elevation)) {
-        throw std::runtime_error("Failed to set min elevation.");
-      }
-      if (!gps_->setCnoThreshold(cno_threshold)) {
-        throw std::runtime_error("Failed to set cno threshold." );
       }
       // Configure each component
       for (const std::shared_ptr<ComponentInterface> & component : components_) {
